252464593ba0e994634b2be7f340087481d96cde
silGreen <verdesilvestro@gmail.com>
2020-09-19 18:43:14 +0200

    lection 15 complete

--- JavaSrc/question/Lection16/InnerMethodClass.java
+++ JavaSrc/question/Lection16/InnerMethodClass.java
@@ -1,5 +1,25 @@
 package JavaSrc.question.Lection16;
 
 public class InnerMethodClass {
-    
+    private int m = (int)(Math.random() * 100);
+    public static void main(String[] args) {
+        InnerMethodClass obj = new InnerMethodClass();
+        obj.doSomething(m, 2*m);
+    }
+
+    public void doSomething(int x, final int y) {
+        int a = x + y;
+        final int b = x - y;
+        class Inner {
+            public void f() {
+                System.out.println("m = " + m); //m is extern attribute so is fine
+                System.out.println("x = " + x); //illegal x is not final attribute
+                System.out.println("y = " + y); // y is a final parameter
+                System.out.println("a = " + a); // illegal a is not final
+                System.out.println("b = " + b); // b is final
+            }
+        }
+        Inner obj1 = new Inner();
+        obj1.f();
+    }
 }
